1ef3cd80290415e7449e6c202e9ddd3b2b8c3cfe
areeba-akbarr <areebaakbar27@gmail.com>
2025-11-28 21:34:58 +0500

    initialize

--- Source1.cpp
+++ Source1.cpp
@@ -1,6 +1,113 @@
 #include <iostream>
-#include <cstdio >
+#include "raylib.h"     
+#include <cstdlib>      
+#include <time.h>       
+#include <cmath>        
+#include <cstdio>
 using namespace std;
+
+//game constants
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 800;
+const int MAX_UFOS = 50;
+const int MAX_SHOTS = 20;
+const int MAX_SPARKS = 100;
+const int NUM_WALLS = 4;
+
+// dimensions for the images
+const int SHIP_W = 80;
+const int SHIP_H = 60;
+const int UFO_W = 60;
+const int UFO_H = 40;
+const int SHOT_W = 16;
+const int SHOT_H = 32;
+
+// speeds and time
+const float BASE_UFO_TIME = 0.05f;
+const float UFO_X_SPEED = 1.0f;
+const float UFO_Y_DROP = 20.0f;
+const float SHIP_MOVE_SPEED = 8.0f;
+const float SHIP_FIRE_DELAY = 0.2f;
+const float TRIPLE_SHOT_DELAY = 1.5f;
+
+
+//level transitions 
+const float FADE_TIME = 0.5f;
+const float HOLD_TIME = 1.5f;
+const float TOTAL_TRANSITION_TIME = FADE_TIME * 2 + HOLD_TIME;
+
+//structd
+
+struct GamerShip {
+    Rectangle hitBox;
+    float speed = SHIP_MOVE_SPEED;
+    int livesLeft = 3;
+    int playerScore = 0;
+    float fireCooldown = 0.0f;
+    float tripleShotCooldown = 0.0f;
+};
+
+//enemy
+struct Ufo {
+    Rectangle hitBox;
+    bool isAlive = false;
+    float fireTimer = 0.0f;
+    const float UFO_FIRE_RATE = 2.0f;
+};
+
+struct LaserShot {
+    Rectangle hitBox;
+    bool isActive = false;
+    float speedVal = 0.0f;
+    bool firedByUfo = false;
+};
+
+struct Spark {
+    Vector2 pos;
+    Vector2 velocity;
+    float size;
+    Color sparkColor;
+    bool isVisible = false;
+};
+
+struct DefenseWall {
+    Rectangle hitBox;
+    int hitPoints = 4;
+};
+
+
+enum GameStatus {
+    INTRO_MENU, HOW_TO_PLAY, IN_GAME, PAUSED_GAME, END_SCREEN, LEVEL_UP
+};
+
+GameStatus gameStatus = INTRO_MENU;
+int currentLevel = 1;
+float ufoMoveTimer = 0.0f;
+float ufoMoveDirection = 1.0f;
+float timeSinceLastUfoShot = 0.0f;
+int highScore = 0;
+int currentUfosAlive = 10;
+int gridRows = 2;
+int gridCols = 5;
+
+// Transition state trackers
+float levelTransitionTimer = 0.0f;
+bool levelResetExecuted = false;
+
+
+// the images we loaded
+Texture2D shipTexture, ufoTexture, playerShotTexture, ufoShotTexture;
+
+// Game Arrays
+GamerShip thePlayer;
+LaserShot allShots[MAX_SHOTS];
+Ufo allUfos[MAX_UFOS];
+DefenseWall allWalls[NUM_WALLS];
+Spark allSparks[MAX_SPARKS];
+
+
+
+
 void LoadScoreFile();
 void SaveScoreFile();
 // function for alien grid setup
