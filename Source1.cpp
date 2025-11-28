8ad5b789124f3a55235ee431e9e9dd938fd6ff25
shahzara2006-star <shahzara2006@gmail.com>
2025-11-28 20:31:20 +0500

    correcting function

--- Source1.cpp
+++ Source1.cpp
@@ -2,43 +2,48 @@
 using namespace std;
 
 
-//function for alien grid setup
+// function for alien grid setup
 
 void SetupUfos(int rows, int cols) {
-	10 currentUfosAlive = 0;
-	11 for (int i = 0; i < MAX_UFOS; i++) {
-		12 allUfos[i].isAlive = false;
-		13
-	}
-	14// we will calculate how many aliens to aluve or spawn
-		15 int maxUfosToSpawn = rows * cols;
-	16 if (maxUfosToSpawn > MAX_UFOS) maxUfosToSpawn = MAX_UFOS;
-	17
-		18 for (int r = 0; r < rows; r++) {
-		19 for (int c = 0; c < cols; c++) {
-			20 int i = r * cols + c;
-			21 if (i >= maxUfosToSpawn) break;
-			22
-				23 allUfos[i].isAlive = true;
-			24 allUfos[i].fireTimer = static_cast <float>(rand() % 500) / 100.0f + 2.0f;
-			25
-				26 float gridWidth = static_cast <float>(cols * (UFO_W + 40) - 40); //setting up grid width
-			27 float startX = (static_cast <float>(SCREEN_WIDTH) - gridWidth) / 2 +
+    currentUfosAlive = 0;
 
-				static_cast <float>(c * (UFO_W + 40));
+    // reset all UFOs
+    for (int i = 0; i < MAX_UFOS; i++) {
+        allUfos[i].isAlive = false;
+    }
 
-			28 float startY = 50 + static_cast <float>(r * (UFO_H + 20));
-			29
-				30 allUfos[i].hitBox = { startX , startY , static_cast <float>(UFO_W), static_cast <
+    // calculate how many aliens to spawn
+    int maxUfosToSpawn = rows * cols;
+    if (maxUfosToSpawn > MAX_UFOS)
+        maxUfosToSpawn = MAX_UFOS;
 
-				float>(UFO_H) };
-			31 currentUfosAlive++;
-			32
-		}
-		33
-	}
-	34
+    // create a grid of UFOs
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) {
+
+            int i = r * cols + c;
+            if (i >= maxUfosToSpawn)
+                break;
+
+            allUfos[i].isAlive = true;
+            allUfos[i].fireTimer = static_cast<float>(rand() % 500) / 100.0f + 2.0f;
+
+            float gridWidth = static_cast<float>(cols * (UFO_W + 40) - 40); // grid width
+            float startX = (static_cast<float>(SCREEN_WIDTH) - gridWidth) / 2 +
+                static_cast<float>(c * (UFO_W + 40));
+
+            float startY = 50 + static_cast<float>(r * (UFO_H + 20));
+
+            allUfos[i].hitBox = { startX, startY,
+                                  static_cast<float>(UFO_W),
+                                  static_cast<float>(UFO_H) };
+
+            currentUfosAlive++;
+        }
+    }
 }
+
+
 
 
 int main()
